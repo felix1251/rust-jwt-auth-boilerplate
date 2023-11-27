4ca9cae5e37433a5192080b74956233ae1176a7a
felix1251 <domhack123@gmail.com>
2023-11-27 21:25:00 +0800

    separate route handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,13 @@
 mod routes;
 mod utils;
 
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Router,
-};
-use routes::{examples, home::home};
-
-// Custom not found route fallback
-async fn fallback() -> (StatusCode, &'static str) {
-    (StatusCode::NOT_FOUND, "ROUTE_NOT_FOUND")
-}
+use routes::create_routes;
 
 pub async fn run() {
     // build our application with a single route
-    let app = Router::new()
-        .route("/", get(home))
-        .nest(
-            "/examples",
-            Router::new()
-                .route("/test_json", post(examples::test_json))
-                .route("/path_vars/:id", get(examples::path_vars))
-                .route("/query_params", get(examples::query_params))
-                .route("/headers", get(examples::headers)),
-        )
-        .fallback(fallback);
+    let services = create_routes();
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, services).await.unwrap();
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,2 +1,26 @@
-pub mod examples;
-pub mod home;
+mod examples;
+mod home;
+
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+
+pub fn create_routes() -> Router {
+    Router::new()
+        .route("/", get(home::home))
+        .nest(
+            "/examples",
+            Router::new()
+                .route("/test_json", post(examples::test_json))
+                .route("/path_vars/:id", get(examples::path_vars))
+                .route("/query_params", get(examples::query_params))
+                .route("/headers", get(examples::headers)),
+        )
+        .fallback(fallback)
+}
+
+async fn fallback() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "ROUTE_NOT_FOUND")
+}
