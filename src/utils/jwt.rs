45106fb0470c01318406a3a4827addb02ce0164e
felix1251 <domhack123@gmail.com>
2023-11-27 16:28:16 +0800

    add needed creates

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -3,8 +3,14 @@ use serde::{Deserialize, Serialize};
 
 /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
 #[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: String,
+pub struct Claims {
     user_id: String,
     exp: usize,
+    iat: usize,
 }
+
+// pub fn create() -> Result<String, StatusCode> {}
+
+// pub fn is_valid() -> Result<bool, StatusCode> {
+//     todo!()
+// }
