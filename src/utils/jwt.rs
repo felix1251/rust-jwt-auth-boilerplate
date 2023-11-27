7f6d706a85f5bee0575ddf023838587abb2f3808
felix1251 <domhack123@gmail.com>
2023-11-27 16:52:48 +0800

    setup axum main

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,4 +1,4 @@
-use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+// use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 
 /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
