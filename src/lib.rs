32e6266c78ada1bb38aac4465f88c248fc9c1e49
felix1251 <domhack123@gmail.com>
2023-11-27 23:21:58 +0800

    simplify lin rub fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod routes;
 mod utils;
 
-use routes::create_routes;
-
 pub async fn run() {
     // build our application with a single route
-    let services = create_routes();
+    let services = routes::create_routes();
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
