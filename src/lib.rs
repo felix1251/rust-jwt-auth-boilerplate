c3ab564c7eda8833bcf861c3fa00f72763048c2a
felix1251 <domhack123@gmail.com>
2023-11-27 17:59:05 +0800

    routes from folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-use axum::{routing::get, Router};
-
 mod routes;
 mod utils;
 
+use axum::{routing::get, Router};
+use routes::home::home;
+
 pub async fn run() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "this is a health check" }));
+    let app = Router::new().route("/", get(home));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,3 +1,3 @@
-// pub async fn home() -> String {
-//     "this is a health check".to_owned()
-// }
+pub async fn home() -> String {
+    "this is a health check".to_owned()
+}
