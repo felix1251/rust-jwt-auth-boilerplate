98245f231aea3104fdeab386645e0702dd6c552c
felix1251 <domhack123@gmail.com>
2023-11-27 15:31:19 +0800

    add jsonwentoken & construct folder structure

---
+++ src/utils/jwt.rs
@@ -0,0 +1,10 @@
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    user_id: String,
+    exp: usize,
+}
