23ab0fd108430f787339343a8c8f2de2a1f839cb
felix1251 <domhack123@gmail.com>
2023-11-27 17:36:24 +0800

    change toml name & separate axum run

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
+use axum::{routing::get, Router};
+
 mod routes;
 mod utils;
+
+pub async fn run() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "this is a health check" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use axum::{routing::get, Router};
+use project::run;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(home));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn home() -> String {
-    "this is a health check".to_owned()
+    run().await
 }
