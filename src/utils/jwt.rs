ccaf2c403ed492add70ccf54e435c84aaece4842
felix1251 <domhack123@gmail.com>
2023-11-28 17:51:15 +0800

    label user claims

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -3,10 +3,10 @@ use serde::{Deserialize, Serialize};
 
 /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Claims {
-    user_id: String,
-    exp: usize,
-    iat: usize,
+pub struct UserClaims {
+    user_id: String, // ID of the user
+    exp: usize,      // Expiration time (as UTC timestamp
+    iat: usize,      // Issued at (as UTC timestamp)
 }
 
 // pub fn create() -> Result<String, StatusCode> {}
