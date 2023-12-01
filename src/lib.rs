37560a339e4716bd0e4a3ab56e1d873981a5bcd9
felix1251 <domhack123@gmail.com>
2023-12-01 17:29:19 +0800

    refactor lik run fn

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,10 @@ mod swagger;
 mod utils;
 
 pub async fn run() {
-    // build our application with a single route
     let services = routes::create_routes();
-
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
 
+    // run our app with hyper, listening globally on port 3000
     axum::serve(listener, services).await.unwrap();
 }
