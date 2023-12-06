42edd0fd05b3ca262896965ef7ecaf86452faf53
felix1251 <domhack123@gmail.com>
2023-12-06 20:24:16 +0800

    update user table with uuid

--- src/models/user.rs
+++ src/models/user.rs
@@ -7,6 +7,7 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
+    pub uuid: Uuid,
     pub fullname: String,
     pub email: String,
     pub encrypted_password: String,
