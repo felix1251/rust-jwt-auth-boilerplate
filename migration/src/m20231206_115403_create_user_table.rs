a4ac2651d08143903bb844ed0cddc49d04f1d506
felix1251 <domhack123@gmail.com>
2023-12-06 20:22:05 +0800

    remove migration commets

--- migration/src/m20231206_115403_create_user_table.rs
+++ migration/src/m20231206_115403_create_user_table.rs
@@ -6,8 +6,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-
         manager
             .create_table(
                 Table::create()
@@ -29,8 +27,6 @@ impl MigrationTrait for Migration {
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-
         manager
             .drop_table(Table::drop().table(User::Table).to_owned())
             .await
