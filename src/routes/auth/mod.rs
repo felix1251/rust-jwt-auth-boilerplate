3a408d498f0351e19a2f2b5949c3d4d0dd2dac46
felix1251 <domhack123@gmail.com>
2023-12-11 06:07:12 +0800

    make sign_in path as post

--- src/routes/auth/mod.rs
+++ src/routes/auth/mod.rs
@@ -1,12 +1,12 @@
 pub mod handlers;
 
 // use crate::middleware::auth_user;
-use axum::{routing::get, Router};
+use axum::{routing::post, Router};
 use sea_orm::DatabaseConnection;
 
 pub fn routes(db: DatabaseConnection) -> Router {
     Router::new()
         // .route_layer(from_fn_with_state(db.clone(), auth_user))
-        .route("/sign-in", get(handlers::sign_in))
+        .route("/sign_in", post(handlers::sign_in))
         .with_state(db)
 }
