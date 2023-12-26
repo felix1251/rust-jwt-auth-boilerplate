eb6adf5a626b317529cb53760121331fbf1835c6
felix1251 <domhack123@gmail.com>
2023-12-26 19:15:44 +0800

    add validator to auths

--- src/utils/password.rs
+++ src/utils/password.rs
@@ -1,17 +1,7 @@
 use super::app_error::AppError;
 use axum::http::StatusCode;
 
-pub fn compare_and_hash_password(
-    password: String,
-    password_confirmation: String,
-) -> Result<String, AppError> {
-    if !password.ne(&password_confirmation) {
-        return Err(AppError::new(
-            StatusCode::UNPROCESSABLE_ENTITY,
-            "Password does not match",
-        ));
-    }
-
+pub fn hash_password(password: String) -> Result<String, AppError> {
     bcrypt::hash(password, 12)
         .map_err(|_err| AppError::new(StatusCode::INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"))
 }
