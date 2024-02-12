027bd4b0074bfc2daa3404caa6d0cd49a8e62263
felix1251 <domhack123@gmail.com>
2024-02-12 18:56:50 +0800

    indent home utoipa::path

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -8,7 +8,14 @@ pub struct HomeSchema {
     message: &'static str,
 }
 
-#[utoipa::path(get, tag = "Home", path = "/", responses((status = 200, description = "Health Check", body = HomeSchema )))]
+#[utoipa::path(
+    get,
+    tag = "Home",
+    path = "/",
+    responses(
+        (status = 200, description = "Health Check", body = HomeSchema )
+    )
+)]
 pub async fn home() -> Json<HomeSchema> {
     Json(HomeSchema {
         message: "This is a health check",
